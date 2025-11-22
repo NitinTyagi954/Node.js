98c3430c34d00649b48d2dc4489275c74a52979b
NitinTyagi954 <nitintyagi7982@gmail.com>
2025-11-22 23:14:40 +0530

    web server ccomplete

--- basic01/web_server.js
+++ basic01/web_server.js
@@ -1,33 +1,33 @@
-// const http = require("http");
-// const server = http.createServer((req,res) => {
-//     console.log("request from server");
-//     res.end("hello from server");
-// });
-// server.listen(8000 , () => {
-//     console.log("server started!");
-// });
-
 const http = require("http");
-const fs = require("fs");
-const myserver = http.createServer((req,res)=>{
-    const log = `${Date.now()} : ${req.url} new request received \n`;
-    fs.appendFile("log.txt" , log , (err,date) => {
-        switch(req.url){
-            case "/" : res.end("I am home page ");
-            break;
-            case  "/about": res.end("i am about page");
-            break;
-            case "/contact": res.end("i am contact page");
-            break;
-            default:
-                res.end("404 not found");
-        }
-    });
-})
+const server = http.createServer((req,res) => {
+    console.log("request from server");
+    res.end("hello from server");
+});
+server.listen(8000 , () => {
+    console.log("server started!");
+});
 
-myserver.listen(8000,()=>{
-    console.log("server workes fine");
-})
+// const http = require("http");
+// const fs = require("fs");
+// const myserver = http.createServer((req,res)=>{
+//     const log = `${Date.now()} : ${req.url} new request received \n`;
+//     fs.appendFile("log.txt" , log , (err,date) => {
+//         switch(req.url){
+//             case "/" : res.end("I am home page ");
+//             break;
+//             case  "/about": res.end("i am about page");
+//             break;
+//             case "/contact": res.end("i am contact page");
+//             break;
+//             default:
+//                 res.end("404 not found");
+//         }
+//     });
+// })
+
+// myserver.listen(8000,()=>{
+//     console.log("server workes fine");
+// })
 // fs.unlink(log.txt);
 
  
