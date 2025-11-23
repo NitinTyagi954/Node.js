c650e5e9319219e7f3072dd30ca4434d93805662
NitinTyagi954 <nitintyagi7982@gmail.com>
2025-11-23 20:00:35 +0530

    starting the express.js

--- basic01/express.js
+++ basic01/express.js
@@ -1,1 +1,12 @@
-// starting the surver 
+const express = require("express");
+const { log } = require("console");
+const app = express();
+app.get('/',(req,res)=>{
+    return res.send("hello from home page");
+});
+app.get("/about" , (req,res)=>{
+    return res.send("hello from about page");
+});
+app.listen(3000,()=>{
+    console.log("surver started!");
+})
